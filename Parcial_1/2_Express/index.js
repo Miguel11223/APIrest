800f72cb406c4900c916c5d0777ce9d82d888464
MiguelDav <terominecraft5@gmail.com>
2025-08-26 21:25:35 -0500

    mark

--- Parcial_1/2_Express/index.js
+++ Parcial_1/2_Express/index.js
@@ -2,13 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
+let data = {
+  message: 'Hello World!'
+};
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send(data.message);
+});
 
 app.post('/', (req, res) => {
-    res.send('Post Hello World!')
-  })
+  res.send('Post Hello World!');
+});
+
+  app.put('/', (req, res) => {
+    const newMessage = req.body.message;
+    if (!newMessage) {
+      return res.status(400).send('El campo "message" es requerido');
+    }
+    data.message = newMessage; // Actualiza el mensaje en la "base de datos"
+    res.send(`Mensaje actualizado: ${newMessage}`);
+  });
+
+  app.delete('/', (req, res) => {
+    data.message = ''; // Simula la eliminación del mensaje
+    res.send('Mensaje eliminado');
+  });
 
 app.listen(port, () => {
   console.log(`El puerto es  ${port}`)
